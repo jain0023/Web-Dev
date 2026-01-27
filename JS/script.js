21df0dcd83b8b20d4b928913202b06db057025b1
Hemant Jain <137457528+jain0023@users.noreply.github.com>
2026-01-28 03:34:51 +0530

    Add files via upload
    starting with js

---
+++ JS/script.js
@@ -0,0 +1,128 @@
+// Conditional statements
+// if statements
+
+// let color = "red";
+
+// if(color === "red"){
+//     console.log("Stop")
+// }
+// if(color === "yellow"){
+//     console.log("Go Slow")
+// }
+// if(color === 'green'){
+//     console.log("Go")
+// }
+
+
+// if else statments
+// let size = "Xl";
+
+// if(size === "Xl"){
+//     console.log("Price is 250")
+// }
+// else if(size === "L"){
+//     console.log("Price is 200")
+// }
+// else if(size === "M"){
+//     console.log("Price is 150")
+// }
+// else{
+//     console.log("Price is 100")
+// }
+
+
+// // nested statements
+// let marks = "100";
+// if(marks >= 33){
+//     console.log("Pass")
+//     if(marks > 80){
+//         console.log("O")
+//     }else{
+//         console.log("A")
+//     }
+// }else{
+//     console.log("F")
+// }
+
+// Practice problem 1
+
+// let word = "am";
+
+// if((word[0] === "a") && (word.length >= 3)){
+//     console.log("Good String")
+// }else{
+//     console.log("Bad String")
+// }
+
+// switch statements
+
+// let color = "Pink";
+
+// switch(color){
+//     case "red":
+//         console.log("STOP")
+//         break;
+//         case "yellow":
+//         console.log("WAIT")
+//         break;
+//         case "green":
+//         console.log("GO")
+//         break;
+//         default:
+//         console.log("broken")
+// }
+
+// practice problem2
+
+// let day = "1";
+
+// switch(day){
+//     case "1":
+//         console.log("Monday")
+//         break;
+//         case "2":
+//         break;
+//         console.log("Tuesday")
+//         case "3":
+//         break;
+//         console.log("Wednesday")
+//         case "4":
+//         break;
+//         console.log("Thrusday")
+//         case "5":
+//         break;
+//         console.log("Friday")
+//         case "6":
+//         break;
+//         console.log("Saturday")
+//         case "7":
+//         break;
+//         console.log("Monday")
+//         default:
+//             console.log("Shit")
+// }
+
+// practice probelm 3
+// let num = 50;
+// if(num % 10 == 0){
+//     console.log("Good")
+// }else{
+//     console.log("bad")
+// }
+
+// practice probelm 
+// let name = prompt("Enter Your Name");
+// let age = prompt("Enter Your Age");
+// console.log(name, "is", age , "years old");
+
+// practice probelm 5
+let num = 122;
+let str = num.toString();
+
+if(str[0] === "9" && str[1] === "9" && str[2] === "9"){
+    console.log("largest three number");
+}else{
+    console.log("not a largest 3 digit number");
+}
+
+
